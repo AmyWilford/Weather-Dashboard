90703749ae5b752692304eca94608fa3296bc734
Amy Wilford <amywilford@Amys-MacBook-Air.local>
2022-09-25 17:20:23 -0400

    added user story and acceptance criteria to README

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// set variables to access HTML elements
+let searchButton = $('#searchButton');
+let searchHistoryEl = $('#searchHistory');
+let cityNameEl= $('#cityName');
+let cityTempEl = $('#cityTemp');
+let cityWindEl = $('#cityWind');
+let cityHumidity = $('#cityHumidity');
+let cityUVIndex = $('#cityUVIndex')
+let weatherTodayEl = $('#weatherToday');
+let weatherForcastEl = $('#weatherForecast');
+
+// Function: Fetch API data and details to daily display & 5-day forecast
+
+// Search Button Event Listener: on click - fetch data from API and return
+
+// on Search Button - localStorage.setItems 
+// on Search BUtton - localStorage.getItems > append Buttons
+
